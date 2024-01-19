263041594534d83753406e4b87ca944469f7ec50
Druserdo <Druserdo@gmail.com>
2024-01-19 20:13:42 +0200

    start

---
+++ HomeWorkLesson5/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("======================================");
+const string name = "Название";
+const string price = "Цена";
+Console.WriteLine($"  {name}          {price}  К-во   Сумма");
+Console.WriteLine()
