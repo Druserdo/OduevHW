244f87839caa77c6ec3336d9fa03c1e54390fd21
Oduev Viktor <druserdo@gmail.com>
2024-01-22 20:56:50 +0200

    готово

---
+++ HomeWorkLesson6/homeWorkLesson6/Program.cs
@@ -0,0 +1,7 @@
+﻿string number = "28";
+byte numberByte =byte.Parse(number);
+nint numberNint = numberByte;
+int numberInt = (int)numberNint;
+long numberLong = (long)numberInt;
+double numberDouble = (double)numberLong;
+Console.WriteLine($"{number}\n{numberByte}\n{numberNint}\n{numberInt}\n{numberLong}\n{numberDouble}");
