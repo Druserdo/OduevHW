63bb9ee29058bd0f2222d8d13a712119136580e6
Oduev Viktor <druserdo@gmail.com>
2024-01-27 23:13:39 +0200

    решил

---
+++ HomeWorkLesson8_take2/Program.cs
@@ -0,0 +1,19 @@
+﻿//1 задача
+int firstEggs = 10 + 8;
+int leftoverEggs = 9;
+int eatenEggs = firstEggs-leftoverEggs;
+if (eatenEggs <= 4)
+{
+    Console.WriteLine($"{eatenEggs} яйца осталось.");
+}
+else
+    Console.WriteLine($"{eatenEggs} яиц осталось.");
+//2 задача
+int magazineWidth = 25;
+int newspaperWidth = magazineWidth - 16;
+Console.WriteLine($"\n\nширина газеты {newspaperWidth}см.");
+//3 задача
+double potato = 44;
+double beet = potato / 4;
+double onion = beet / 2;
+Console.WriteLine($"\n\nПовар потратил {onion} лука.");
