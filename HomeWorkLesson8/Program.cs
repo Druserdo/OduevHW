bf25b996f3b3f4c05078b6fdeabf28eee7d35c39
Oduev Viktor <druserdo@gmail.com>
2024-01-27 22:44:13 +0200

    Решение

---
+++ HomeWorkLesson8/Program.cs
@@ -0,0 +1,14 @@
+﻿//1 задача
+int petyaBooks = 10;
+int alyoshaBooks = petyaBooks - 4;
+int vanyaBooks = alyoshaBooks + 2;
+int allBooks = petyaBooks + alyoshaBooks + vanyaBooks;
+Console.WriteLine($"Петя взял {petyaBooks} книг.");
+Console.WriteLine($"Алеша взял {alyoshaBooks} книг.");
+Console.WriteLine($"Ваня взял {vanyaBooks} книг.");
+Console.WriteLine($"Всего мальчики взяли {allBooks} книг");
+//2 задача
+int firstBasket = 23;
+int secondBasket = firstBasket - 11;
+int totalApples= firstBasket + secondBasket;
+Console.WriteLine($"\n\nВсего яблок {totalApples}");
---
+++ OduewCW/CWLesson9/Program.cs
@@ -0,0 +1,18 @@
+﻿// a<<2 (два раза поделить на 2), a<<3 (три раза поделить на 2)
+//a>>2(два раза умножить на 2), a>>3(три раза умножить на 2)
+//int a = 10;
+//Console.WriteLine(a >> 3);
+//Console.WriteLine(a << 2);
+//int b = 2;
+//Console.WriteLine(a & b);
+//int a = 5;
+//int b = 22;
+//Console.WriteLine(a | b);
+Console.Write("Введите число:");
+int a=int.Parse(Console.ReadLine());
+if (a % 2 >= 1)
+    {
+     Console.WriteLine("Не чтное");
+    }
+else
+    Console.WriteLine("Четное");
