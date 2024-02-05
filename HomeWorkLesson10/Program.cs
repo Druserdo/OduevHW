c3eb2c1041dee50acfd01a883c098d01fb43fea9
Oduev Viktor <druserdo@gmail.com>
2024-02-05 20:46:11 +0200

    Готово

---
+++ HomeWorkLesson10/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите радиус окружности:");
+double r=double.Parse(Console.ReadLine());
+double length=2*Math.PI*r;
+double S = Math.PI * (r * r);
+Console.WriteLine($"Длинна окружности с радиусом {r} равна {length:F2}");
+Console.WriteLine($"Площадь окружности с радиусом {r} равна {S:F2}");
