87f67c17d9c7a55ad94410a7f3fc9f0b554d6841
Oduev Viktor <druserdo@gmail.com>
2024-02-16 21:21:00 +0200

    решил

---
+++ HomeWorkLesson11_take3/HomeWorkLesson11_takeMonth/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("Введите номер месяца:");
+int numMonth=int.Parse(Console.ReadLine());
+if (numMonth == 12 || numMonth == 2 || numMonth == 3)
+{
+    Console.Write("Введите температуру:");
+    int t = int.Parse(Console.ReadLine());
+    switch (numMonth)
+    {
+        case 12: Console.WriteLine("Декабрь"); break;
+        case 1: Console.WriteLine("Январь"); break;
+        case 2: Console.WriteLine("Февраль"); break;
+    }
+    if (t > 0)
+        Console.WriteLine("Дождливая зима!");
+}
+else if (numMonth >= 1 && numMonth <= 12)
+{
+    switch (numMonth)
+    {
+        case 3: Console.WriteLine("Март"); break;
+        case 4: Console.WriteLine("Апрель"); break;
+        case 5: Console.WriteLine("Май"); break;
+        case 6: Console.WriteLine("Июнь"); break;
+        case 7: Console.WriteLine("Июль"); break;
+        case 8: Console.WriteLine("Август"); break;
+        case 9: Console.WriteLine("Сентябрь"); break;
+        case 10: Console.WriteLine("Октябрь"); break;
+        case 11: Console.WriteLine("Ноябрь"); break;
+    }
+}
+else
+    Console.WriteLine("Такого месяца нет!");
---
+++ HomeWorkLesson11_take3/Program.cs
@@ -0,0 +1,14 @@
+﻿Random random = new Random();
+int num1=random.Next(0, 6);
+Console.WriteLine("Введите число от 0 до 5:");
+int numUser=int.Parse(Console.ReadLine());
+if (numUser == num1)
+{
+    Console.WriteLine(num1);
+    Console.WriteLine("Победа!");
+}
+else
+{
+    Console.WriteLine(num1);
+    Console.WriteLine("Попробуйте снова(");
+}
