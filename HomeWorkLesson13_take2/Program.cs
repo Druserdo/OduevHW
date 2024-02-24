ebafe7203951b25d58e6af3be03780e022a062ae
Oduev Viktor <druserdo@gmail.com>
2024-02-24 18:38:24 +0200

    gotovo

--- HomeWorkLesson13_take2/Program.cs
+++ HomeWorkLesson13_take2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Random random = new Random();
+//int n;
+//do
+//{
+//    n = random.Next(1, 11);
+//    Console.Write(n + " ");
+//}
+//while (n != 7);
+
+int num;
+int sum = 0;
+do
+{
+    Console.Write("Введите число:");
+    num = int.Parse(Console.ReadLine());
+    sum += num;
+}
+while (num !=0 );
+Console.Write(sum);
