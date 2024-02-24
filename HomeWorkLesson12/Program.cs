c60c5fc9b62c214287621ba5e1f426bb304f564b
Oduev Viktor <druserdo@gmail.com>
2024-02-24 18:24:01 +0200

    решил

--- HomeWorkLesson12/Program.cs
+++ HomeWorkLesson12/Program.cs
@@ -4,3 +4,10 @@ Console.Write("Введите второе число:");
 int secondNum=int.Parse(Console.ReadLine());
 Console.Write("Выберете операцию:+,-,*,/");
 char operation = Char.Parse(Console.ReadLine());
+switch(operation)
+{
+    case '+':Console.WriteLine($"{firstNum}+{secondNum}={firstNum+secondNum}");break;
+    case '-':Console.WriteLine($"{firstNum}-{secondNum}={firstNum - secondNum}"); break;
+    case '*':Console.WriteLine($"{firstNum}*{secondNum}={firstNum * secondNum}"); break;
+    case '/':Console.WriteLine($"{firstNum}/{secondNum}={firstNum / secondNum}"); break;
+}
