81ae8cfe16c57dac52931fa863a4e2f81955fa8c
Oduev Viktor <druserdo@gmail.com>
2024-03-10 11:33:40 +0200

    решил

---
+++ HomeWorkLesson15_take2/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] mas = new int[10];
+Random random = new Random();
+int ewenNumbers = 0;
+for (int i = 0; i < mas.Length; i++)
+{
+    mas[i] = random.Next(10,100);
+}
+for (int i = 0;i < mas.Length; i++)
+{
+    Console.Write(mas[i]+" ");
+}
+Console.WriteLine();
+for (int i = 0; i<mas.Length; i++)
+{
+    if (mas[i]%2== 0)
+    ewenNumbers++;
+}
+Console.WriteLine($"Четных чиел в массиве {ewenNumbers}");
