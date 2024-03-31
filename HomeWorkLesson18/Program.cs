55ecf6260a3d9f5d0541a45324e12758266d507c
Oduev Viktor <druserdo@gmail.com>
2024-03-31 18:38:19 +0200

    gotovo

---
+++ HomeWorkLesson18/Program.cs
@@ -0,0 +1,23 @@
+﻿string[] forbiddenWords = { "слово1", "слово2", "слово3" };
+string sentence = "";
+while (true)
+{
+    Console.Write("Введите слово (для завершения введите 'конец'): ");
+    string word = Console.ReadLine();
+for (int i = 0; i < forbiddenWords.Length; i++)
+    {
+        if (word == forbiddenWords[i])
+        {
+            word = "(скрыто)"; 
+            break;
+        }
+    }
+    if (word == "конец")
+    {
+        break;
+    }
+
+    sentence += word + " ";
+}
+
+Console.WriteLine("Сформированное предложение: " + sentence);
