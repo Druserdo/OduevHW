6924d2290a55731ee8871e75e03da99f2888f3eb
Oduev Viktor <druserdo@gmail.com>
2024-03-31 18:51:58 +0200

    gotovo

---
+++ HomeWorkLesson18_take2/Program.cs
@@ -0,0 +1,15 @@
+﻿//1
+//Console.WriteLine("Введите числа через ','");
+//string offer=Console.ReadLine();
+//string[] mas = offer.Split(",");
+//int sum = 0;
+//for (int i = 0; i < mas.Length; i++)
+//{
+//    sum+= int.Parse(mas[i]);
+//}
+//Console.Write(sum);
+//2
+Console.WriteLine("Введите предложение:");
+string offer=Console.ReadLine();
+string[] mas = offer.Split(" ");
+Console.WriteLine($"Количество слов: {mas.Length}");
