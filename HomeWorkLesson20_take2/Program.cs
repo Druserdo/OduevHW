f850e915af684c9bf4e38896ac5d3bbded821aa2
Oduev Viktor <druserdo@gmail.com>
2024-04-07 13:10:09 +0200

    gotovo

---
+++ HomeWorkLesson20_take2/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] mas1 = { 1, 11, 23, 4 };
+
+foreach (int i in mas1) Console.Write(i+" ");
+int awgMas(int[] mas)
+{
+    int sum = 0;
+    for (int i = 0; i < mas.Length; i++)
+    {
+        sum += mas[i];
+
+    }
+    int sumAvg = sum/mas.Length;
+    return sumAvg;
+}
+Console.WriteLine();
+Console.WriteLine($"Среднее арифметическое массива равно:{awgMas(mas1)}");
