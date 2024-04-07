1e2c6c4bedf09eb55e1113d378b5c1b5088d5bc3
Oduev Viktor <druserdo@gmail.com>
2024-04-07 12:24:47 +0200

    gotovo

---
+++ HomeWork20_1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите первое число:");
+int num1=int.Parse(Console.ReadLine());
+Console.Write("Введите второе число:");
+int num2=int.Parse(Console.ReadLine());
+int addNumbers(int a, int b)
+{
+    int summ=a+b;
+    return summ;
+}
+Console.WriteLine($"Сумма чисел равна:{addNumbers(num1, num2)}");
