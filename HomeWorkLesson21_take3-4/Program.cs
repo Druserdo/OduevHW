e878fc9206f1da7e52d9c58250012781d8e2e6cc
Oduev Viktor <druserdo@gmail.com>
2024-04-21 20:28:06 +0200

    g

---
+++ HomeWorkLesson21_take3-4/Program.cs
@@ -0,0 +1,11 @@
+﻿string directoryPath = @"C:\Users\Mark\Desktop\Учеба\С#\project\HomeWorkLesson21_take3-4\obj\Debug\net8.0";
+GetDir(directoryPath);
+Console.Write("Введите папку для перехода:");
+string namDir=Console.ReadLine();
+Console.WriteLine($"{directoryPath}\\{namDir}");
+void GetDir(string directoryPath)
+{
+    
+    DirectoryInfo directoryInfo = new DirectoryInfo(directoryPath);
+    Console.WriteLine("Текущая дирректория: " + directoryInfo.FullName);
+}
