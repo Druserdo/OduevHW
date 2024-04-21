640dfbc2b9c9f231b5bd725b25142205291497c2
Oduev Viktor <druserdo@gmail.com>
2024-04-21 20:34:51 +0200

    gg

--- HomeWorkLesson21_take3-4/Program.cs
+++ HomeWorkLesson21_take3-4/Program.cs
@@ -1,11 +1,13 @@
 ﻿string directoryPath = @"C:\Users\Mark\Desktop\Учеба\С#\project\HomeWorkLesson21_take3-4\obj\Debug\net8.0";
-GetDir(directoryPath);
+Console.WriteLine(GetDir(directoryPath));
 Console.Write("Введите папку для перехода:");
 string namDir=Console.ReadLine();
-Console.WriteLine($"{directoryPath}\\{namDir}");
-void GetDir(string directoryPath)
+directoryPath += $"\\{namDir}";
+Console.WriteLine(GetDir(directoryPath));
+string GetDir(string dirPath)
 {
     
-    DirectoryInfo directoryInfo = new DirectoryInfo(directoryPath);
-    Console.WriteLine("Текущая дирректория: " + directoryInfo.FullName);
+    DirectoryInfo directoryInfo = new DirectoryInfo(dirPath);
+   string currentDir="Текущая дирректория: " + directoryInfo.FullName;
+    return currentDir;
 }
