e6c20b6f25ee63e2dd770bfcc0cf8000b2b16b7d
Oduev Viktor <druserdo@gmail.com>
2024-04-21 17:25:48 +0200

    gotovo

---
+++ HomeWorkLesson21_take2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите путь:");
+string path=Console.ReadLine();
+FileInfo fileInfo = new FileInfo(path);
+if (fileInfo.Exists)
+{
+    Console.WriteLine($"Имя файла: {fileInfo.Name}");
+    Console.WriteLine($"Время создания: {fileInfo.CreationTime}");
+    Console.WriteLine($"Размер: {fileInfo.Length}");
+}
