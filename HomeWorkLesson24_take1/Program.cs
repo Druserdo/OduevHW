21987ab0c4f4b2ea5a5299c1d0d62917f93b7d7e
Oduev Viktor <druserdo@gmail.com>
2024-04-28 17:41:36 +0200

    gotowo

---
+++ HomeWorkLesson24_take1/Program.cs
@@ -0,0 +1,27 @@
+﻿Main();
+bool IsPalindrome(string word)
+    {
+        int length = word.Length;
+        for (int i = 0; i < length / 2; i++)
+        {
+            if (word[i] != word[length - i - 1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+     void Main()
+    {
+    Console.Write("Введите слово:");
+    string input = Console.ReadLine()!.ToLower();
+        if (IsPalindrome(input))
+        {
+            Console.WriteLine("Is a palindrom");
+        }
+        else
+        {
+            Console.WriteLine("Not a palindrom");
+        }
+    }
