3c52f5361bc286676123e1baeb9fd80d15e8554b
Oduev Viktor <druserdo@gmail.com>
2024-05-01 19:14:43 +0200

    дубль1

---
+++ HomeWorkLesson25_take1/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace HomeWorkLesson25_take1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBox1 = new ListBox();
+            label1 = new Label();
+            listBox2 = new ListBox();
+            listBox3 = new ListBox();
+            listBox4 = new ListBox();
+            button1 = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            label7 = new Label();
+            listBox5 = new ListBox();
+            panel1 = new Panel();
+            button2 = new Button();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.Font = new Font("Segoe UI", 12F, FontStyle.Italic);
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 21;
+            listBox1.Location = new Point(24, 66);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(148, 109);
+            listBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F);
+            label1.Location = new Point(22, 42);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 21);
+            label1.TabIndex = 2;
+            // 
+            // listBox2
+            // 
+            listBox2.Font = new Font("Segoe UI", 12F, FontStyle.Italic);
+            listBox2.FormattingEnabled = true;
+            listBox2.ItemHeight = 21;
+            listBox2.Location = new Point(212, 254);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(148, 109);
+            listBox2.TabIndex = 3;
+            // 
+            // listBox3
+            // 
+            listBox3.Font = new Font("Segoe UI", 12F, FontStyle.Italic);
+            listBox3.FormattingEnabled = true;
+            listBox3.ItemHeight = 21;
+            listBox3.Location = new Point(395, 254);
+            listBox3.Name = "listBox3";
+            listBox3.Size = new Size(148, 109);
+            listBox3.TabIndex = 4;
+            // 
+            // listBox4
+            // 
+            listBox4.Font = new Font("Segoe UI", 12F, FontStyle.Italic);
+            listBox4.FormattingEnabled = true;
+            listBox4.ItemHeight = 21;
+            listBox4.Location = new Point(578, 254);
+            listBox4.Name = "listBox4";
+            listBox4.Size = new Size(148, 109);
+            listBox4.TabIndex = 5;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            button1.Location = new Point(0, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(86, 30);
+            button1.TabIndex = 6;
+            button1.Text = "Записать";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F);
+            label2.Location = new Point(22, 223);
+            label2.Name = "label2";
+            label2.Size = new Size(74, 21);
+            label2.TabIndex = 7;
+            label2.Text = "Задача 1";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F);
+            label3.Location = new Point(212, 223);
+            label3.Name = "label3";
+            label3.Size = new Size(74, 21);
+            label3.TabIndex = 8;
+            label3.Text = "Задача 2";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F);
+            label4.Location = new Point(395, 223);
+            label4.Name = "label4";
+            label4.Size = new Size(74, 21);
+            label4.TabIndex = 9;
+            label4.Text = "Задача 3";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F);
+            label5.Location = new Point(578, 223);
+            label5.Name = "label5";
+            label5.Size = new Size(74, 21);
+            label5.TabIndex = 10;
+            label5.Text = "Задача 4";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 12F);
+            label6.Location = new Point(22, 21);
+            label6.Name = "label6";
+            label6.Size = new Size(124, 21);
+            label6.TabIndex = 11;
+            label6.Text = "Текущая задача";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(25, 181);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(147, 23);
+            dateTimePicker1.TabIndex = 12;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label7.Location = new Point(178, 182);
+            label7.Name = "label7";
+            label7.Size = new Size(116, 21);
+            label7.TabIndex = 13;
+            label7.Text = "Выберите дату";
+            // 
+            // listBox5
+            // 
+            listBox5.Font = new Font("Segoe UI", 12F, FontStyle.Italic, GraphicsUnit.Point, 204);
+            listBox5.FormattingEnabled = true;
+            listBox5.ItemHeight = 21;
+            listBox5.Location = new Point(24, 254);
+            listBox5.Name = "listBox5";
+            listBox5.Size = new Size(148, 109);
+            listBox5.TabIndex = 14;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Location = new Point(182, 66);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(86, 71);
+            panel1.TabIndex = 15;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            button2.Location = new Point(0, 39);
+            button2.Name = "button2";
+            button2.Size = new Size(86, 29);
+            button2.TabIndex = 0;
+            button2.Text = "Удалить";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(760, 450);
+            Controls.Add(panel1);
+            Controls.Add(listBox5);
+            Controls.Add(label7);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(listBox4);
+            Controls.Add(listBox3);
+            Controls.Add(listBox2);
+            Controls.Add(label1);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            Text = "Планировщик 1.0";
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private ListBox listBox1;
+        private Label label1;
+        private ListBox listBox2;
+        private ListBox listBox3;
+        private ListBox listBox4;
+        private Button button1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private DateTimePicker dateTimePicker1;
+        private Label label7;
+        private ListBox listBox5;
+        private Panel panel1;
+        private Button button2;
+    }
+}
