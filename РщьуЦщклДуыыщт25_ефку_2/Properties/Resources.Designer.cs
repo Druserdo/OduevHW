d30d80857ea3d74d7fae1ac271153c5990616e68
Oduev Viktor <druserdo@gmail.com>
2024-05-01 19:35:24 +0200

    gthtbvtyjdfk&

--- РщьуЦщклДуыыщт25_ефку_2/Properties/Resources.Designer.cs
+++ РщьуЦщклДуыыщт25_ефку_2/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace РщьуЦщклДуыыщт25_ефку_2.Properties {
+namespace HomeWorkLesson25_take2.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace РщьуЦщклДуыыщт25_ефку_2.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("РщьуЦщклДуыыщт25_ефку_2.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("HomeWorkLesson25_take2.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
