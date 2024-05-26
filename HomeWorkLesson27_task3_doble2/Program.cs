2a46f30053a78bf9ccc1ce30bfc7756be6f50f76
Oduev Viktor <druserdo@gmail.com>
2024-05-26 15:17:52 +0200

    number

---
+++ HomeWorkLesson27_task3_doble2/Program.cs
@@ -0,0 +1,44 @@
+﻿
+class Numbers
+{
+    public void CalculateAverage(int[] mas)
+    {
+        int avg;
+        int sum = 0;
+        for (int i = 0; i < mas.Length; i++)
+        {
+            sum += mas[i];
+        }
+        avg = sum / mas.Length;
+    }
+    public bool IsPrime(int num)
+    {
+        int count = 0;
+        bool res;
+        for (int i = 1; i < num; i++)
+        {
+            if (num % i != 0) count++;
+        }
+        if (count == 0) return res = true;
+        else return res = false;
+    }
+    public bool IsEven(int num)
+    {
+        bool res;
+        if (num % 2 == 0)
+        {
+            return res = true;
+        }
+        else return res = false;
+    }
+    public bool IsOdd(int num)
+    {
+        bool res;
+        if (num % 2 == 0)
+        {
+            return res = false;
+        }
+        else return res = true;
+    }
+
+}
